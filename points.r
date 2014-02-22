f713d85ecce4730d381afc552739bc6b290bdf1e
Carleton <carleton.coffrin@nicta.com.au>
2014-02-22 18:09:25 +1100

    updated points.r to show completion relative to last session

--- points.r
+++ points.r
@@ -10,6 +10,8 @@ bounds = c(0,400)
 targets = c(42,84,126,182,224)
 goals = rep(goal, length(targets))
 
+previous_rate = 788/4073
+#print(previous_rate)
 
 args = commandArgs(TRUE)
 if(length(args) == 3){
@@ -28,11 +30,16 @@ data21 = read.csv(dataFile21,header=FALSE,sep=",",stringsAsFactors=TRUE)
 #dataOrdered = data[order(data)]
 #print(dataOrdered)
 
+proportional_target = length(data0)*previous_rate
+#print(proportional_target)
+
 counts = rep(0, length(targets))
 for(i in 1:length(targets)){
 	counts[i] = max(which(data0 > targets[i]))
 }
+#print(length(data0))
 #print(counts)
+
 
 
 plotCircle = function(x,y,text){
@@ -62,12 +69,13 @@ fileName = "points.pdf"
 pdf(fileName, pointsize=16, width=12, height=8)
     par(mar=c(4.5,4.5,0.5,0.5))
 	#main="Parts Completed by Students"
-	plot(c(bounds[1]+20,bounds[2]-20), c(50,length(data0)-150), ylab="Total Number of Students", xlab="Points Awarded", main="", type='n') #, panel.first=grid()
+	plot(c(bounds[1]+20,bounds[2]-15), c(50,length(data0)-150), ylab="Total Number of Students", xlab="Points Awarded", main="", type='n') #, panel.first=grid()
 	
 	for(i in 1:length(targets)){
 		points(c(targets[i],targets[i]), c(counts[i],-500), type='l', lty=1, lw=3, col=rgb(0.0,0.0,0.0,0.2))
 		if(counts[i]<goal){
-			points(c(targets[i],targets[i]), c(goal,counts[i]), type='l', lw=3, col=targetColor2) #col=rgb(0.0,0.0,0.0,0.5)
+		    #red line to target
+			#points(c(targets[i],targets[i]), c(goal,counts[i]), type='l', lw=3, col=targetColor2) #col=rgb(0.0,0.0,0.0,0.5)
 		}
 		#abline(v=targets[i], lw=1, col=targetColor1, lty=2)
 		
@@ -76,13 +84,15 @@ pdf(fileName, pointsize=16, width=12, height=8)
 	#points(targets, goals-offset, pch=21, cex=3, lw=2, col="black", bg="white", type='p')
 	#text(targets, goals-offset, 1:length(targets))
 	
-	points(c(-100,tail(targets,1)), c(goal,goal), type='l', lw=3, col=rgb(0.0,0.0,0.0,1.0), lty=1)
+	points(c(-100,tail(targets,1)), c(proportional_target,proportional_target), type='l', lw=3, col=rgb(0.0,0.0,0.0,1.0), lty=1)
+	#our dream
+	#points(c(-100,tail(targets,1)), c(goal,goal), type='l', lw=3, col=rgb(0.0,0.0,0.0,1.0), lty=1)
 	#points(c(tail(targets,1),tail(targets,1)), c(goal,-500), type='l', lw=3, col=targetColor3, lty=1)
 	
-	points(data0, 1:length(data0), type='l', lw=4, col=barColor0)
-	points(data7, 1:length(data7), type='l', lw=4, col=barColor7)
-	points(data14, 1:length(data14), type='l', lw=4, col=barColor14)
 	points(data21, 1:length(data21), type='l', lw=4, col=barColor21)
+    points(data14, 1:length(data14), type='l', lw=4, col=barColor14)
+    points(data7, 1:length(data7), type='l', lw=4, col=barColor7)
+	points(data0, 1:length(data0), type='l', lw=4, col=barColor0)
 	
 	plotCircle(targets, counts-offset, 1:length(targets))
 	#plotCircle(targets, rep(0, length(targets)), 1:length(targets))
@@ -91,5 +101,5 @@ pdf(fileName, pointsize=16, width=12, height=8)
 	
 
 
-	legend("topright", c("Present", "1 Week Ago", "2 Weeks Ago", "3 Weeks Ago", "Our Dream", "Assignment"), cex=1.0, pch=c(-1,-1,-1,-1,-1, 21), lwd=c(4,4,4,4,3,2), lty=c(1,1,1,1,1,-1), col=c(barColor0,barColor7,barColor14,barColor21,"black","black"), bg="white");
+	legend("topright", c("Present", "1 Week Ago", "2 Weeks Ago", "3 Weeks Ago", "Last Session", "Assignment"), cex=1.0, pch=c(-1,-1,-1,-1,-1, 21), lwd=c(4,4,4,4,3,2), lty=c(1,1,1,1,1,-1), col=c(barColor0,barColor7,barColor14,barColor21,"black","black"), bg="white");
 dev.off()
